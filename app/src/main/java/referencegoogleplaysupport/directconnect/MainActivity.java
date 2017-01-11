6272b1fa4820d7d83ee744a30f0ff56b3c8dbb4c
psuhsien <psu.hsien@gmail.com>
2017-01-11 10:05:22 +0900

    Push Eevery thing

---
+++ app/src/main/java/referencegoogleplaysupport/directconnect/MainActivity.java
@@ -0,0 +1,49 @@
+package referencegoogleplaysupport.directconnect;
+
+import android.content.Context;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.format.Formatter;
+import android.widget.TextView;
+
+import java.net.InetAddress;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WifiManager wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+
+        if (wifi.isWifiEnabled()) {
+
+            String IPBin = Integer.toBinaryString(wifi.getConnectionInfo().getIpAddress());
+            String IP = Integer.parseInt(IPBin.substring(0,8), 2) + "." +
+                    Integer.parseInt(IPBin.substring(8,16), 2) + "." +
+                    Integer.parseInt(IPBin.substring(16,24), 2) + "." +
+                    Integer.parseInt(IPBin.substring(24,32), 2);
+
+
+            ((TextView) findViewById(R.id.WiFi_SSID)).setText("SSID: " + wifi.getConnectionInfo().getSSID());
+            ((TextView) findViewById(R.id.WiFi_IP)).setText("IP: " + IP);
+
+
+        }
+        else {
+            // make dialog to ask enable wifi
+
+        }
+
+        // start waiting connection
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+    }
+}
